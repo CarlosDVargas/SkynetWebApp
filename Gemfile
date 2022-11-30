3a47074f79ad25f16be32c15afe0bc95c717118e
Carlos D. Vargas Varela <carlos.vargas@ucrso.info>
2022-11-30 00:47:05 -0600

    Se agrega gema devise, se agrega inicio de sesión

--- Gemfile
+++ Gemfile
@@ -73,3 +73,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "devise", "~> 4.8"
+
+gem "byebug", "~> 11.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
+
 end
