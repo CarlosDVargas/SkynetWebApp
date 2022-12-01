533cef0474c65e310f37bae5e4d72ac717e187d5
Adriana AH <adrianapah10@gmail.com>
2022-12-01 15:37:57 -0600

    validate unit of batch

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,4 +4,5 @@ class Batch < ApplicationRecord
 
   validates :current_operation, presence: true
   validates :product_id, presence: true
+  validates :units, presence: true, numericality: {greater_than_or_equal_to: 100, less_than_or_equal_to:1500}
 end
