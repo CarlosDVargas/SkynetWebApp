814260a65523c52beccde3e20df2e95f6076cf0f
Adriana AH <adrianapah10@gmail.com>
2022-12-01 21:34:02 -0600

    validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates_presence_of :first_name, :last_name, :username, :email
+  validates_length_of :first_name, :last_name, :username, minimum: 3
 end
