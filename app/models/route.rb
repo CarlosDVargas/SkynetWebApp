f1a49b5799078bdd79a3e9ebfc3994877d4a54d0
Carlos D. Vargas Varela <carlos.vargas@ucrso.info>
2022-12-02 15:18:02 -0600

    Se agrega detalles de operaciones

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,4 +5,14 @@ class Route < ApplicationRecord
   validates :description, presence: true
   validates :code, presence: true, uniqueness: true
   validates :country_id, presence: true
+
+  def get_products
+    products = []
+    operations.each do |operation|
+      operation.batches.each do |batch|
+        products << batch.product
+      end
+    end
+    products
+  end
 end
