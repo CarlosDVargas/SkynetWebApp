e86272c2d22d604ae0f96605494586610d96fa03
Carlos D. Vargas Varela <carlos.vargas@ucrso.info>
2022-12-01 22:30:58 -0600

    Se agrega el seguimiento de país.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  enum role: %i[:engineer, :admin, :manager, :regular]
+  def set_role
+    self.role ||= :regular
+  end
+
+
   validates_presence_of :first_name, :last_name, :username, :email
   validates_length_of :first_name, :last_name, :username, minimum: 3
 end
